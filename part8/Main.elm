455e24c6b79590a1121a165e8aeff54c2397db31
Feihong <feihong.hsu@gmail.com>
2017-03-24 21:59:10 -0500

    Solution for part 8

--- part8/Main.elm
+++ part8/Main.elm
@@ -136,11 +136,12 @@ type Msg
 
 decodeResponse : Value -> Msg
 decodeResponse json =
-    -- TODO use decodeValue to decode the response into a Msg.
-    --
-    -- Hint: look at the definition of Msg and
-    -- the definition of responseDecoder
-    HandleSearchError (Just "TODO decode the response!")
+    case Json.Decode.decodeValue responseDecoder json of
+        Ok results ->
+            HandleSearchResponse results
+
+        Err errMessage ->
+            HandleSearchError (Just errMessage)
 
 
 port githubSearch : String -> Cmd msg
